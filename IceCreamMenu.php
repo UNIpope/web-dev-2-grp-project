23387874b4c0ca7330703eb6546b353a70f850d1
Buch <25643076+buchita@users.noreply.github.com>
2018-10-21 15:39:17 +0100

    Add files via upload

---
+++ IceCreamMenu.php
@@ -0,0 +1,55 @@
+<!-- 
+	Website: Ice Cream Store
+	Purpose: Display the menu of the shop
+	Authors: Jack Duggan + Buchita Gitchamnan	
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IceCreamHome</title>
+
+	<meta charset="utf-8"> 
+
+	<!-- rsponsive-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- include CDN(Content Delivery Network) MaxCDN provides CDN support for Bootstrap's CSS and JavaScript. You must also include jQuery-->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="IceCreamMenu.css">
+</head>
+<body>
+	<!-- navbar, set the default navbar with fixed position at the top-->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	 	<div class="container">
+		    <!--<div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span> 
+		      </button>
+		      <a class="navbar-brand" href="#">Logo</a>
+		    </div>
+
+		    <div class="collapse navbar-collapse" id="myNavbar">-->
+		    <ul class="nav navbar-nav navbar-right">
+		    	<li><a href="#home">HOME</a></li>
+		        <li><a href="#about">ABOUT</a></li>
+		        <li><a href="#scoop">SCOOP</a></li>
+		        <li><a href="#topping">TOPPING</a></li>
+		        <li><a href="#contact">CONTACT</a></li>
+		        <li><a href="#user"><span class="glyphicon glyphicon-user"></span></a></li>
+		        <li><a href="#search"><span class="glyphicon glyphicon-search"></span></a></li>
+		        <li><a href="#cart"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+		    </ul>
+		    <!--</div>-->
+	  	</div>
+	</nav>
+</body>
+</html>
