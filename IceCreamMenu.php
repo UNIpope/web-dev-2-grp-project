aeb7b001783c88c4863b9c1b5fcaf3834bc40382
Buch <25643076+buchita@users.noreply.github.com>
2018-10-21 18:31:45 +0100

    Add files via upload

--- IceCreamMenu.php
+++ IceCreamMenu.php
@@ -24,7 +24,9 @@
 
 	<link rel="stylesheet" type="text/css" href="IceCreamMenu.css">
 </head>
-<body>
+
+<!-- for scrolling and the navbar is keeping track-->
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
 	<!-- navbar, set the default navbar with fixed position at the top-->
 	<nav class="navbar navbar-default navbar-fixed-top">
 	 	<div class="container-fluid">
@@ -44,17 +46,54 @@
 	  	</div>
 	</nav>
 
-	<div class="container">
+	<!-- add banner-->
+	<div class="container" id="home">
 		<img src="header-scoops.png" width="100%">	
 
+		<!-- name of the store-->
 		<div class="jumbotron jum-name">
-			<h1>My CONEection</h1>
-			
+			<h1>My CONEection</h1>	
 		</div>	
 
-		<div class="jumbotron">
-			<h3 ><mark>Info</mark></h3>
+		<!-- about section starts-->
+		<div class="jumbotron" id="about">
+			<h2 ><mark>About</mark></h2>
+			<p>Ice cream (derived from earlier iced cream or cream ice) is a sweetened frozen food typically eaten as a snack or dessert. 
+			It is usually made from dairy products, such as milk and cream, and often combined with fruits or other ingredients and flavors. 
+			It is typically sweetened with sugar or sugar substitutes. Typically, flavourings and colourings are added in addition to stabilizers. 
+			The mixture is stirred to incorporate air spaces and cooled below the freezing point of water to prevent detectable ice crystals from forming. 
+			The result is a smooth, semi-solid foam that is solid at very low temperatures (< 2 °C or 35 °F). It becomes more malleable as its temperature increases.
+
+			The meaning of the phrase "ice cream" varies from one country to another. 
+			Phrases such as "frozen custard", "frozen yogurt", "sorbet", "gelato", and others are used to distinguish different varieties and styles. 
+			In some countries, such as the United States, the phrase "ice cream" applies only to a specific variety, and most governments regulate the commercial use of the various terms according to the relative quantities of the main ingredients, notably the amount of cream.
+			Products that do not meet the criteria to be called ice cream are labelled "frozen dairy dessert" instead.
+			In other countries, such as Italy and Argentina, one word is used for all variants. 
+			Analogues made from dairy alternatives, such as goat's or sheep's milk, or milk substitutes (e.g., soy milk or tofu), are available for those who are lactose intolerant, allergic to dairy protein, or vegan.</p>
 		</div>
+
+		<div class="container text-center bg-grey" id="scoop">
+			<h2 >Scoopies</h2>
+			<div class="row text-center">
+				<div class="jumbotron">
+					<div class="thumbnail">
+					<img src="bananaNstraw.png">
+					<p>Banan and Strawberry</p>
+				</div>
+				</div>
+				
+				<div class="thumbnail">
+					<img src="bubblegum.png">
+					<p>Bubble gum</p>
+				</div>
+				<div class="thumbnail">
+					<img src="chocchip.png">
+					<p>Chocolate chips</p>
+				</div>
+			</div>
+			
+		</div>
+
 	</div>
 
 
