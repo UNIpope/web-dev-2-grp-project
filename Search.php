5da5e1b3c70ade9c0fead4f30115030ae6762176
UNIpope <UNIpope@users.noreply.github.com>
2018-11-21 12:56:46 +0000

    Add files via upload

---
+++ Search.php
@@ -0,0 +1,106 @@
+<!-- 
+	Website: Ice Cream Store
+	Purpose: Display the menu of the shop
+	Authors: Jack Duggan + Buchita Gitchamnan	
+	Reference: www.w3schools.com
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Search</title>
+
+	<meta charset="utf-8"> 
+
+	<!-- rsponsive-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- include CDN(Content Delivery Network) MaxCDN provides CDN support for Bootstrap's CSS and JavaScript. You must also include jQuery-->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="IceCreamMenu.css">
+
+
+
+
+
+</head>
+
+<!-- for scrolling and the navbar is keeping track-->
+<body>
+	<?php
+		//navbar
+		require('header2.php');
+	?>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	
+	<div class="container">
+		<form class="card card-sm" method="post" >
+			<div class="card-body row no-gutters align-items-center">
+				<div class="col">
+					<input class="form-control form-control-lg form-control-borderless" type="search" name="sr" placeholder="Enter name of ice cream.">
+				</div>
+				<!--end of col-->
+				<br/>
+				<div class="col-auto">
+					<button class="btn btn-lg btn-success" type="submit" name="sub" style="float: center; background-color: pink; color: black;">Search</button>
+				</div>
+				<!--end of col-->
+			</div>
+		</form>
+	</div>
+	
+	
+	<?php	
+		if (isset($_POST['sub'])) {
+			
+			//create a connect to the database
+			$con = mysqli_connect("localhost", "root", "","ice_cream_db");
+
+			//check connection
+			if (mysqli_connect_errno())
+			{
+				#fail to connects
+				echo "failed to connect".mysqli_connect_errno();
+			}
+
+
+			$sr = $_POST["sr"]; 
+			//select where name = sr
+			$all = "SELECT name, des, pic, price FROM ice_cream where name like '%".$sr."%'";
+			
+			//do the query and print 
+			$re = mysqli_query($con, $all);
+			
+			
+			echo "<table border='1'>
+			<tr>
+			<th>name</th>
+			<th>des</th>
+			<th>price</th>
+			<th>pic</th>
+			</tr>";
+			
+			while($row = mysqli_fetch_array($re))
+			{
+				echo "<tr>";
+				echo "<td>" . $row['name'] . "</td>";
+				echo "<td>" . $row['des'] . "</td>";
+				echo "<td>" . $row['price'] . "</td>";
+				echo "<td>" . $row['pic'] . "</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+		}
+	?>
+	
+</body>
+</html>
---
+++ header2.php
@@ -0,0 +1,98 @@
+<!-- 
+	Website: Ice Cream Store
+	Purpose: Display the header
+	Authors: Jack Duggan + Buchita Gitchamnan	
+	reference: www.w3schools.com
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Header</title>
+
+
+	<meta charset="utf-8"> 
+
+	<!-- rsponsive-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- include CDN(Content Delivery Network) MaxCDN provides CDN support for Bootstrap's CSS and JavaScript. You must also include jQuery-->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+	<style type="text/css">
+		body
+		{
+    		font-family: "Courier New";
+		}
+		.navbar
+		{
+		    /*set the background colour pink*/
+		    background-color: pink;
+		    margin-bottom: 0; 
+		    border: 0;
+		    font-size: 16px !important;
+		    letter-spacing: 4px;
+		}
+		.navbar li a, .navbar .navbar-brand
+		{
+		    /*change colour of the text*/
+		    color: #fff !important;
+		}
+		.navbar-nav li a:hover, .navbar-nav li.active a
+		{
+		    /*hovering = letters change to pink*/
+		    color: pink !important;
+		    /*when hovering over  background turns white*/
+		    background-color: #fff !important;
+		}
+		.navbar-default .navbar-toggle
+		{
+		    border-color: transparent;
+		    color: #fff !important;
+		}
+	</style>
+
+		
+	
+
+
+</head>
+
+
+<!-- for scrolling and the navbar is keeping track-->
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+<!--navbar, set the default navbar with fixed position at the top-->
+	<div class="collapse navbar-collapse" id="mynavbar">
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<li><a href="IceCreamMenu.php" style="float: right;"><span class="glyphicon glyphicon-home"></span></a></li>
+		</nav>
+	</div>
+	
+		<nav class="navbar navbar-default navbar-fixed-top">
+	 	<div class="container-fluid">
+
+
+	 		<!-- title -->
+	 		<div class="navbar-header">
+	 			<a class="navbar-brand" href="#">CONEction</a>
+	 		</div>
+
+	 		<!-- home -->
+	 		<div class="collapse navbar-collapse" id="mynavbar">
+			    <ul class="nav navbar-nav navbar-right">
+			        <li><a href="IceCreamMenu.php" style="float: right;"><span class="glyphicon glyphicon-home"></span></a></li>
+			    </ul>
+		   	</div>
+	  	</div>
+	</nav>
+
+	
+
+</body>
+</html>
